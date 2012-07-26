bd816241c3665a9f53083b5f826a3a0e184bc32d
kianvest <kianvest@gmail.com>
2012-07-26 13:54:44 -0700

    Renamed array to increase sanity; reduced scope of the array previously known as array

--- js/stream.js
+++ js/stream.js
@@ -1,48 +1,7 @@
-var n = 20, // number of layers
-    m = 200, // number of samples per layer
-    data0 = d3.layout.stack().offset("wiggle")(stream_layers(n, m)),
-    data1 = d3.layout.stack().offset("wiggle")(stream_layers(n, m)),
-    color = d3.interpolateRgb("#aad", "#556");
 
-var width = 960,
-    height = 500,
-    mx = m - 1,
-    my = d3.max(data0.concat(data1), function(d) {
-      return d3.max(d, function(d) {
-        return d.y0 + d.y;
-      });
-    });
-
-var area = d3.svg.area()
-    .x(function(d) { return d.x * width / mx; })
-    .y0(function(d) { return height - d.y0 * height / my; })
-    .y1(function(d) { return height - (d.y + d.y0) * height / my; });
-
-var vis = d3.select("#chart")
-  .append("svg")
-    .attr("width", width)
-    .attr("height", height);
-
-vis.selectAll("path")
-    .data(data0)
-  .enter().append("path")
-    .style("fill", function() { return color(Math.random()); })
-    .attr("d", area);
-
-function transition() {
-  d3.selectAll("path")
-      .data(function() {
-        var d = data1;
-        data1 = data0;
-        return data0 = d;
-      })
-    .transition()
-      .duration(2500)
-      .attr("d", area);
-}
 
 /* Inspired by Lee Byron's test data generator. */
-function stream_layers(n, m, o) {
+var stream_layers = function(n, m, o) {
   if (arguments.length < 3) o = 0;
   function bump(a) {
     var x = 1 / (.1 + Math.random()),
@@ -59,18 +18,84 @@ function stream_layers(n, m, o) {
       for (i = 0; i < 5; i++) bump(a);
       return a.map(stream_index);
     });
-}
+};
 
 /* Another layer generator using gamma distributions. */
-function stream_waves(n, m) {
+var stream_waves = function(n, m) {
   return d3.range(n).map(function(i) {
     return d3.range(m).map(function(j) {
         var x = 20 * j / m - i / 3;
         return 2 * x * Math.exp(-.5 * x);
       }).map(stream_index);
     });
+};
+
+
+
+var n = 5, // number of layers
+    m = 200, // number of samples per layer
+    generators = [stream_layers,stream_waves],
+    gIndex = 0,
+    generator = generators[gIndex],
+    data0 = d3.layout.stack().offset("wiggle")(stream_layers(n, m)),
+    data1 = d3.layout.stack().offset("wiggle")(stream_waves(n, m)),
+    color = d3.interpolateRgb("#aad", "#556");
+
+var width = 960,
+    height = 500,
+    mx = m - 1,
+    my = d3.max(data0.concat(data1), function(d) {
+      return d3.max(d, function(d) {
+        return d.y0 + d.y;
+      });
+    });
+
+var area = d3.svg.area()
+    .x(function(d) { return d.x * width / mx; })
+    .y0(function(d) { return height - d.y0 * height / my; })
+    .y1(function(d) { return height - (d.y + d.y0) * height / my; });
+
+function render_streamgraph(color_scheme) {
+  var vis = d3.select("#chart")
+    .append("svg")
+      .attr("width", width)
+      .attr("height", height);
+
+    vis.selectAll("path")
+    .data(data0)
+  .enter().append("path")
+    .style("fill", function(d,i) { 
+      return color_scheme[i]; 
+    })
+    .attr("d", area);
+  }
+ 
+function alter_color(color_scheme) {
+  d3.select("#chart svg").selectAll("path")
+  .transition()
+  .duration(1000)
+  .style('fill',function(d,i) { 
+      return color_scheme[i]; 
+    });
+}
+
+
+function transition() {
+  gIndex = ++gIndex >= generators.length ? 0 : gIndex;
+  generator = generators[gIndex];
+  
+  d3.selectAll("path")
+      .data(function() {
+        var d = data1;
+        data1 = data0;
+        return data0 = d;
+      })
+    .transition()
+      .duration(2500)
+      .attr("d", area);
 }
 
 function stream_index(d, i) {
   return {x: i, y: Math.max(0, d)};
 }
+
