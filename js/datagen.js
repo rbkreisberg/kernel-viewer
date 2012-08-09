d3762a2c5bb6a9fa061b6d49626de7dac728dfbc
kianvest <kianvest@gmail.com>
2012-08-08 22:52:47 -0700

    Added heart rendering and random data generation

---
+++ js/datagen.js
@@ -0,0 +1,22 @@
+function dataGen(size, xrange, yrange){
+	var points = [];
+	for (var i = 0; i < size; i++){
+		//replace with science.js Gaussian random distribution?
+		var x = Math.random() * xrange;
+		var y = Math.random() * yrange;
+		points.push([x,y]);
+	}
+	return points;
+}
+
+function plotPoints(points){
+	var svg = d3.select("body").append("svg");
+	 svg.selectAll(".circle")
+	  	.data(points)
+	     .enter().append("circle")
+	 .attr("class", "circle")
+	 .attr("cx", function(point) {return point[0];})
+	 .attr("cy", function(point) {return point[1];})
+	 .attr("r", 1)
+	 .style("fill", "black");
+}
